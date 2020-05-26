04348da7a2a5e1f25381bdd1ef53b3d32985943d
Дмитрий Чернецкий <0639651043w@gmail.com>
2020-05-26 22:12:18 +0300

    1

---
+++ Interface2/Account_Balanse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface2
+{
+    class Account_Balanse
+    {
+        public string account_name;
+        public int starting_balanse;
+        public Account_Balanse(string account_name, int starting_balanse) 
+        {
+            this.account_name = account_name; 
+            this.starting_balanse = starting_balanse; 
+        }
+
+        public int current_balanse;
+
+        public int withdraw()
+        {
+
+            Console.WriteLine("Введите сумму снятия ");
+            int sum1 = int.Parse(Console.ReadLine());
+            current_balanse = starting_balanse-sum1;
+            Console.WriteLine("Баланс после снятия " + current_balanse);
+            return current_balanse;
+        }
+
+        public int deposit(int current_balanse)
+        {
+            Console.WriteLine("Введите сумму зачисления ");
+            int sum2 = int.Parse(Console.ReadLine());
+            current_balanse = current_balanse + sum2;
+            Console.WriteLine("Баланс после зачисления " + current_balanse);
+            return current_balanse;
+
+        }
+
+
+
+    }
+}
---
+++ Interface2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interface2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВВедите текущий баланс");
+            int x = int.Parse(Console.ReadLine());
+            Account_Balanse account_Balanse = new Account_Balanse("Dima", x);
+            var q = account_Balanse.withdraw();
+            account_Balanse.deposit(q);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
