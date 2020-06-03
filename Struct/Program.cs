649f58289e1ffd8b7370815158c5f693c93b6fa8
Дмитрий Чернецкий <0639651043w@gmail.com>
2020-06-04 00:17:07 +0300

    1

---
+++ Struct/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    struct Circle
+    {
+        public double L;
+        public Circle(double L)
+        {
+            this.L = L;
+        }
+        public double Radius()
+        {
+            double R = (L / (2 * 3.14));
+            return R;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(25);
+            
+            Console.WriteLine(circle.Radius());
+
+
+            Console.ReadLine();
+        }
+    }
+}
